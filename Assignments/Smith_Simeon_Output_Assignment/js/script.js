16a81a6f5ae15cac0e01e746870b6d8c8b58792c
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-04 20:47:45 -0800

    Output Assignment - Display age and birth info.
    Display age, place of birth, year of birth and month of birth.

--- Assignments/Smith_Simeon_Output_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Output_Assignment/js/script.js
@@ -8,18 +8,21 @@
 // Output Assignment
 
 
-var firstName = "Simeon "; //My first name.
+var firstName = "Simeon"; //My first name.
 var lastName = "Smith"; //My last name.
 var yearBirth = 1988; //My year of birth.
+var monthBirth = "November"; //My month of birth.
 var yearCurrent = new Date().getFullYear(); //Current year.
 var placeBirth = "Anchorage, Alaska"; //My location of birth.
 var generMale = true; //My gender.
 var relationshipStatus = "married"; //My relationship status.
 var parent = true; //My parental status.
 var grandparent = false; //My grandparental status.
+var siblingNames = ["Michael", "Marielle", "Gabriella", "Julianna", "John", "Anna"]
 
-console.log("My name is " + firstName + lastName + "."); //Outputting to console.log my name.
+console.log("My name is " + firstName + " " + lastName + "."); //Outputting to console.log my name.
 
 var age = yearCurrent - yearBirth; //Calculating my age from the current yearn and my year of birth.
 
-console.log(age); //Testing age output.
+console.log("I was born in " + placeBirth + " in " + yearBirth + " and I will be " + age + " this " + monthBirth + ".");
+
