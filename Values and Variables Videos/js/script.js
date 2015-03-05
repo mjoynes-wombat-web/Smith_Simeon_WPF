465388aeed5279838593480617a1b6bece8166c6
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-04 18:45:23 -0800

    Values and Variables Video - yearOfBirth
    Variable defined and displayed in the consol. Renamed fodler from
    "Values and Variables" to "Values and Variables Videos" folder.

---
+++ Values and Variables Videos/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Simeon on 3/4/2015.
+ */
+
+var yearOfBirth = 1988;
+
+console.log(yearOfBirth);
--- Values and Variables/js/script.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Simeon on 3/4/2015.
- */
-alert("Hello Professor Lewis from JavaScript!");
