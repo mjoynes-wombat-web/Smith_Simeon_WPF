210b8070794300bc8c66bb8a1c051ba5ebd8df69
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-04 19:58:34 -0800

    Output Assignment - Calculated age.
    Calculated age based on current year minus year of birth and output to
    console.log to insure proper operation.

--- Assignments/Smith_Simeon_Output_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Output_Assignment/js/script.js
@@ -11,12 +11,15 @@
 var firstName = "Simeon "; //My first name.
 var lastName = "Smith"; //My last name.
 var yearBirth = 1988; //My year of birth.
-var currentYear = new Date().getFullYear(); //Current year.
+var yearCurrent = new Date().getFullYear(); //Current year.
 var placeBirth = "Anchorage, Alaska"; //My location of birth.
 var generMale = true; //My gender.
-var relationshipStatus = "married";
-var parent = true;
-var grandparent = false;
+var relationshipStatus = "married"; //My relationship status.
+var parent = true; //My parental status.
+var grandparent = false; //My grandparental status.
 
-console.log("My name is " + firstName + lastName + ".");
+console.log("My name is " + firstName + lastName + "."); //Outputting to console.log my name.
 
+var age = yearCurrent - yearBirth; //Calculating my age from the current yearn and my year of birth.
+
+console.log(age); //Testing age output.
