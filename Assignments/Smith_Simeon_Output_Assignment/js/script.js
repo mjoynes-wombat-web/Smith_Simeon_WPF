84bbac5ff229a0555730a9e1087cc1e3dff1c52d
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-04 20:56:21 -0800

    Output Assignment - Gender and relationships.
    Output boolean statement about gender, boolean parental statuses,
    marriage status and sibling number.

--- Assignments/Smith_Simeon_Output_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Output_Assignment/js/script.js
@@ -14,15 +14,21 @@ var yearBirth = 1988; //My year of birth.
 var monthBirth = "November"; //My month of birth.
 var yearCurrent = new Date().getFullYear(); //Current year.
 var placeBirth = "Anchorage, Alaska"; //My location of birth.
-var generMale = true; //My gender.
+var genderMale = true; //My gender.
 var relationshipStatus = "married"; //My relationship status.
 var parent = true; //My parental status.
 var grandparent = false; //My grandparental status.
+var numSiblings = 6;
 var siblingNames = ["Michael", "Marielle", "Gabriella", "Julianna", "John", "Anna"]
 
 console.log("My name is " + firstName + " " + lastName + "."); //Outputting to console.log my name.
 
 var age = yearCurrent - yearBirth; //Calculating my age from the current yearn and my year of birth.
 
-console.log("I was born in " + placeBirth + " in " + yearBirth + " and I will be " + age + " this " + monthBirth + ".");
+console.log("I was born in " + placeBirth + " in " + yearBirth + " and I will be " + age + " this " + monthBirth + "."); // Outputting birth location, year of birth, age and month of birth.
 
+console.log("It is " + genderMale + " that I am male."); //Outputting gender.
+
+console.log("I am " + relationshipStatus + " and it is " + parent + " that I am a parent and " + grandparent + " that I am a grandparent."); //Outputting relationship statuses.
+
+console.log("I have " + numSiblings + "."); //Outputting number of siblings.
