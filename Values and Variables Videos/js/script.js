70e9cdb632b3652072a8dc6dd6fa6b839c030cca
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-04 19:14:08 -0800

    Values and Variables Video - Printed out array.
    Printed out array in console.log.

--- Values and Variables Videos/js/script.js
+++ Values and Variables Videos/js/script.js
@@ -2,4 +2,6 @@
  * Created by Simeon on 3/4/2015.
  */
 
-var siblingNames = ["Michael", "Marielle", "Gabriella", "Julianna", "John", "Anna"];
+var siblingNames = ["Michael", "Marielle", "Gabriella", "Julianna", "John", "Anna"]; //0, 1, 2, 3, 4, 5
+
+console.log(siblingNames);
