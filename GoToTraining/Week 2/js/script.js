cb6b4b20d003c1ffbd7c457f80fd7d9201f8d8d4
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-09 18:04:04 -0700

    GTT2 Final File
    Finished file for GoToTraining Week 2

--- GoToTraining/Week 2/js/script.js
+++ GoToTraining/Week 2/js/script.js
@@ -9,4 +9,31 @@ GTT Week 2 Tutorial
  */
 
 //Alert Test
-alert("Do I work!!!");
+//alert("Do I work!!!");
+
+//Variables
+// TODO:Add vars.
+
+var addressStreet = "808 E. 6th Ave.";
+var addressCityStateZip = "Ellensburg, WA 98926";
+var distanceToParents = 0.5;
+var isThereConstruction = true;
+var myFirstName,
+    myLastName,
+    myAge;
+
+
+//Prompts
+// TODO:Add prompts.
+
+myFirstName = prompt("Please enter your first name:");
+myLastName = prompt("Please enter your last name:");
+myAge = Number(prompt("How old are you?"));
+
+
+//Outputs
+// TODO:Add some outputs.
+
+console.log("Your name is " + myFirstName + " " + myLastName +".");
+console.log("My age in 2 years will be " + (myAge + 2) + ".");
+
