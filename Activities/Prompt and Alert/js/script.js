4e4615d99964d88e0651418a645e6ddb4857055a
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-10 14:29:53 -0700

    Screencast: Prompts and Alerts
    The tutorial for Screencast: Prompts and Alerts.

---
+++ Activities/Prompt and Alert/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Simeon on 3/10/2015.
+ */
+
+// Simeon Smith
+// Scalable Data Infrastructures
+// C201503 Term
+// Prompt and Alert Activity
+
+//Prompt and Alert
+
+/*
+var yearOfBirth = prompt("Enter your year of birth:"); //Prompts for input with string.
+
+console.log(yearOfBirth);
+*/
+
+var width = prompt("Please enter the width of your rectangle:"); //Prompt for width of rectangle.
+var height = prompt("Please enter the height:"); //Prompt for height.
+
+var area = width * height; //Area calculation from user input.
+
+//console.log("The area of the rectangle is " + area + ".");
+
+alert("The area of the rectangle is " + area + " sq. in.");
