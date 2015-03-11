9139424a09008b4fb2a25715f9567042bff3aec7
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-10 19:31:24 -0700

    Expression Assignment: Avg. MPG Outputs
    Output of the average mpg variables.

--- Assignments/Smith_Simeon_Expressions_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Expressions_Assignment/js/script.js
@@ -8,7 +8,7 @@
 /*VARIABLES*/
 
 var title = "Checking Your MPG History.\n",     //Title for the top of the prompts.
-    warning = "\n Please only enter numbers.",   //Warning to only use numbers.
+    warning = "\nPlease only enter numbers.",   //Warning to only use numbers.
     last4Mpg = [22, 19, 16, 25],                //Last 4 MPG results from fill ups.
     last4Price = [2.50, 2.25, 2.15, 1.92],      //Last 4 prices per gallon from fill ups.
     gallons,                                    //Variable place holder for user gallons prompt.
@@ -31,33 +31,49 @@ price = Number(prompt(title + "What was the total cost of this fill up?" + warni
 
 /*FIRST CALCULATIONS*/
 
-currentMpg = miles / gallons;
-currentPrice = price / gallons;
-last5PriceAvg = last4Price[0] + last4Price[1] + last4Price[2] + last4Price[3] + currentPrice;
-last5PriceAvg /= 5;
+currentMpg = miles / gallons;                                                                   //Calculating mpg from user input.
+currentPrice = price / gallons;                                                                 //Calculating price per gallon from user input.
+last5PriceAvg = last4Price[0] + last4Price[1] + last4Price[2] + last4Price[3] + currentPrice;   //Adding price to prep for division.
+last5PriceAvg /= 5;                                                                             //Dividing to find the average price.
 
 
-/*FIRST CONSOLE OUTPUTS */
+/*FIRST CONSOLE OUTPUTS*/
 
 console.log("Your current mpg is " + currentMpg + " mpg and cost you $" + currentPrice + " per gallon." );  //Outputting the MPG and price per gallon.
 console.log("The price per gallon average from the last 5 fill ups is $" + last5PriceAvg + ".");            //Outputting the average price per gallon for the last 5 fill ups.
 
 
-/*SECOND CALCULATIONS */
+/*SECOND CALCULATIONS*/
 
 priceChange = currentPrice - last5PriceAvg; //Calculating the change in price.
 currentPrice += priceChange;                //Calculating the possible price for the next fill up if the trend continues.
 
 
-/* SECOND CONSOLE OUTPUTS */
+/*SECOND CONSOLE OUTPUTS*/
 
 console.log("The is a " + priceChange + " change for this fill up. If this trend continues your price will be $" + currentPrice + " next time.");  //Outputting the price change and the possible next price based on the trend.
 
 
 /*THIRD CALCULATIONS*/
 
-last5MpgAvg = last4Mpg[0] + last4Mpg[1] +last4Mpg[2] +last4Mpg[3] + currentMpg;
-last5MpgAvg /= 5;
+last5MpgAvg = last4Mpg[0] + last4Mpg[1] +last4Mpg[2] +last4Mpg[3] + currentMpg; //Adding mpg to prep for division.
+last5MpgAvg /= 5;                                                               //Dividing to find the average mpg.
+
+
+/*THIRD CONSOLE OUTPUTS*/
+
+console.log("Your mpg average from the last 5 fillups is " + last5MpgAvg + " mpg.");
+
+
+/*FOURTH CALCULATION*/
+
+mpgChange = currentMpg - last5MpgAvg;
+currentMpg += mpgChange;
+
+
+/*FOURTH CALCULATION*/
+
+console.log("This is a " + mpgChange + " mpg change for this fillup. If this trend continues your mpg will be " + currentMpg + " next time.");
 
 /*TESTING SECTION*/
 
@@ -73,5 +89,7 @@ last5MpgAvg /= 5;
 
 //console.log(currentPrice);                      //Price difference calculation test.
 
-//console.log(last5MpgAvg);                     //Test of last 5 price average calculation.
+//console.log(last5MpgAvg);                       //Test of last 5 price average calculation.
+
+//console.log(mpgChange, currentMpg);             //Test of mpg change calculations.
 
