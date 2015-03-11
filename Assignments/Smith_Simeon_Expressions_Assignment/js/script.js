fdea4d24de5bd639aa71e562a8797817af2c690f
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-10 17:48:53 -0700

    Expressions Assignment: Variable setup.
    Finished setting up variables and tested the new line at the end of the
    title var.

--- Assignments/Smith_Simeon_Expressions_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Expressions_Assignment/js/script.js
@@ -3,3 +3,18 @@
 // C201503 Term
 // Expressions Assignment
 // 3-10-2015
+
+var title = "Checking Your MPG History.\n",
+    last4Mpg = [22, 19, 16, 25],
+    last4Price = [2.50, 2.25, 2.15, 1.92],
+    gallons,
+    price,
+    miles,
+    currentMpg,
+    currentPrice,
+    last5PriceAvg,
+    priceChange,
+    last5MpgAvg,
+    mpgChange;
+
+alert(title+"This is a test.");
