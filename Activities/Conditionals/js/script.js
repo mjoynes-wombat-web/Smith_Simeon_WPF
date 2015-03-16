28b20d9b0b347dc9c47dbc2d241e4945198dd8a3
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-16 15:17:33 -0700

    Coditionals: Conditionals Examples
    Example of a Conditional.

---
+++ Activities/Conditionals/js/script.js
@@ -0,0 +1,27 @@
+/*
+Simeon Smith
+Scalable Data Infrastructures
+C201503 Term
+Conditionals Activity
+March 21, 2013
+ */
+
+/*
+var temp = prompt("What is the temperature?");
+
+if(temp > 65){
+    alert("It's warm!");
+}else{
+    alert("It is cold!");
+};
+*/
+
+var oldEnough = true;
+
+//If the child is old enough, print to the console "You can ride!"
+
+if(oldEnough){
+    //Code to perform if condition is true.
+    console.log("You can ride the roller coaster!");
+}
+console.log("I always print.");
