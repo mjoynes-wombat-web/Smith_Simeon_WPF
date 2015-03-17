de26c63a81cb8813ccef5c606cf26e86e6f5f734
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-17 14:55:03 -0700

    Conditionals Assignment: Ternary
    Setup some ternary operators.

--- Assignments/Smith_Simeon_Conditionals_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Conditionals_Assignment/js/script.js
@@ -9,12 +9,12 @@ March 21, 2013
 /*
 Requirements
 
-a.  An example of 2 different arithmetic operators (addition and subtraction,
+//a.  An example of 2 different arithmetic operators (addition and subtraction,
     addition and multiplication, subtraction and division, etc.).
-b.  2 conditional statements – A least one must contain an else if statement
-c.  At least 1 ternary conditional statement must be used
+/b.  2 conditional statements – A least one must contain an else if statement
+/c.  At least 1 ternary conditional statement must be used
 d.  At least 1 Logical Operator && , || or !
-e.  Must contain at least 3 user prompts that are used in calculations
+/e.  Must contain at least 3 user prompts that are used in calculations
 f.  All prompts must be validated
 
 Instructions
@@ -33,12 +33,16 @@ Instructions
 
 /* VARIABLES */
 
-var monthlyPay = 0,     //Users monthly pay
+var monthlyPay,     //Users monthly pay
     monthlyBills,   //Users monthly bills
-    housePrice,     //House price
+    itemPrice,     //House price
     interestRate,   //Loan interest rate
     loanTerm,        //Length of loan
-    downPayment;    //House down payment
+    downPayment,    //House down payment
+    monthlyExcess,  //Extra money after bills
+    monthlyPayment, //Monthly payment amount
+    interestPaid,   //Total interest paid
+    totalPaid;      //Total amount paid
 
 
 /*FUNCTIONS*/
@@ -49,7 +53,7 @@ function monthlyPayPrompt(){    //User prompt asking for monthly pay and validat
     monthlyPayValidate();
 }
 
-function monthlyPayValidate(){  //Monthly pay validation and alert if not a number. Recalls prompt.
+function monthlyPayValidate(){  //Monthly pay validation and alert if not a number and recalls prompt, else calls monthly bills prompt.
     if(isNaN(monthlyPay)){
         alert("You didn't enter a number for your pay. \nPlease enter only a number.");
         monthlyPayPrompt();
@@ -65,7 +69,7 @@ function monthlyBillsPrompt(){  //User prompt asking for monthly bills and valid
     monthlyBillsValidate();
 }
 
-function monthlyBillsValidate(){  //Monthly bills validation and alert if not a number. Recalls prompt.
+function monthlyBillsValidate(){  //Monthly bills validation and alert if not a number and recalls prompt, else calls house price prompt.
     if(isNaN(monthlyBills)){
         alert("You didn't enter a number for your expenses. \nPlease enter only a number.");
         monthlyBillsPrompt();
@@ -77,13 +81,13 @@ function monthlyBillsValidate(){  //Monthly bills validation and alert if not a 
 
 //House Price Functions
 function housePricePrompt(){    //User prompt asking for the house price and validation call.
-    housePrice = Number(prompt("What is the price of the house? \nPlease enter only numbers."));
+    itemPrice = Number(prompt("What is the price of the item to be financed? \nPlease enter only numbers."));
     housePriceValidate();
 }
 
-function housePriceValidate(){  //House price validation and alert if not a number. Recalls prompt.
-    if(isNaN(housePrice)){
-        alert("You didn't enter a number for the price of the house. \nPlease enter only a number.");
+function housePriceValidate(){  //House price validation and alert if not a number and recalls prompt, else calls interest rate prompt.
+    if(isNaN(itemPrice)){
+        alert("You didn't enter a number for the price of the item. \nPlease enter only a number.");
         housePricePrompt();
         housePriceValidate();
     }else{
@@ -92,12 +96,12 @@ function housePriceValidate(){  //House price validation and alert if not a numb
 }
 
 //Interest Rate Functions
-function interestRatePrompt(){  //User prompt asking for the loan interest rate.
-    interestRate = Number(prompt("What is the loan interest rate? \nPlease enter the interest rate as a whole number e.g. 50%=50."));
+function interestRatePrompt(){  //User prompt asking for the loan interest rate and validation call.
+    interestRate = (Number(prompt("What is the loan interest rate? \nPlease enter the interest rate as a whole number e.g. 50%=50.")))/100;
     interestRateValidate();
 }
 
-function interestRateValidate(){    //Interest rate validation and alert if not a number. Recalls prompt.
+function interestRateValidate(){    //Interest rate validation and alert if not a number and recalls prompt, else calls loan term prompt.
     if(isNaN(interestRate)){
         alert("You didn't enter a number for the interest rate. \nPlease enter only numbers.");
         interestRatePrompt();
@@ -108,36 +112,36 @@ function interestRateValidate(){    //Interest rate validation and alert if not 
 }
 
 //Loan Term Functions
-function loanTermPrompt(){  //User prompt asking for the loan term length.
+function loanTermPrompt(){  //User prompt asking for the loan term length and validation call.
     loanTerm = Number(prompt("What is the loan term length? \nPlease enter the term in months and only as a number."));
     loanTermValidate();
 }
 
-function loanTermValidate(){    //Interest rate validation and alert if not a number. Recalls prompt.
+function loanTermValidate(){    //Interest rate validation and alert if not a number and recalls prompt, else continue to down payment prompt.
     if(isNaN(loanTerm)){
         alert("You didn't enter a number for the loan term. \nPlease enter only numbers.");
         loanTermPrompt();
         loanTermValidate();
     }else{
-        downPaymentPrompt();
+        downPaymentConfirm();
     }
 }
 
 //Down Payment Functions
-function downPaymentConfirm(){   //User prompt asking if there was a down payment.
-    downPayment = confirm("Was a down payment made on the house? Click OK if yes and Cancel if no.");
+function downPaymentConfirm(){   //User prompt asking if there was a down payment and then calls the down payment prompt
+    downPayment = confirm("Was a down payment made on the item? Click OK if yes and Cancel if no.");
     downPaymentPrompt();
 }
 
-function downPaymentPrompt(){   //User prompt asking for the amount of the down payment.
+function downPaymentPrompt(){   //User prompt asking for the amount of the down payment if the was one else
     if(downPayment) {
-        downPayment = Number(prompt("How much was the down payment on the house? \nPlease enter only numbers."));
+        downPayment = Number(prompt("How much was the down payment on the item? \nPlease enter only numbers."));
         downPaymentValidation();
     }else{
     }
 }
 
-function downPaymentValidation() {  //Down payment validation and alert if not a number. Recalls downPaymentPrompt.
+function downPaymentValidation() {  //Down payment validation and alert if not a number and recalls downPaymentPrompt else
     if(isNaN(downPayment)) {
         alert("You didn't enter a number for the loan term. \nPlease enter only numbers.");
         downPaymentPrompt();
@@ -146,8 +150,18 @@ function downPaymentValidation() {  //Down payment validation and alert if not a
 
 }
 
-//monthlyPayPrompt();
-//monthlyBillsPrompt();
-//interestRatePrompt();
-//loanTermPrompt();
-downPaymentConfirm();
+monthlyPayPrompt();
+
+
+//interestPaid = (Premium * (interest/12*(Math.pow(((1+interest)/12), term))))/((Math.pow((1+interest/12), term)-1)
+
+monthlyPayment = itemPrice * ((interestRate/12*(Math.pow((1+interestRate/12), loanTerm)))/((Math.pow((1+interestRate/12), loanTerm)-1)));
+totalPaid = monthlyPayment * loanTerm;
+interestPaid = totalPaid - itemPrice;
+
+
+
+(confirm("would you like to see how much your monthly payment would be?")) ? console.log("Your monthly payment will be $" + monthlyPayment + ".") : ""  ;
+(confirm("Would you like to see how much you will pay?")) ? console.log("You will pay $" + totalPaid + " total for this item.") : "" ;
+(confirm("Would you like to see how much you will pay in interest?")) ? console.log("You will pay $" + interestPaid + " in interest.") : "" ;
+
