f8339744897871c2b495ee83a16ad1629dc3d1d4
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-16 20:08:13 -0700

    Conditionals Assignment:Setup
    Initial file setup for the Conditionals assignment.

---
+++ Assignments/Smith_Simeon_Conditionals_Assignment/js/script.js
@@ -0,0 +1,31 @@
+/*
+Simeon Smith
+Scalable Data Infrastructures
+C201503 Term
+Conditionals Assignment
+March 21, 2013
+ */
+
+/*
+Requirements
+
+a.  An example of 2 different arithmetic operators (addition and subtraction,
+    addition and multiplication, subtraction and division, etc.).
+b.  2 conditional statements – A least one must contain an else if statement
+c.  At least 1 ternary conditional statement must be used
+d.  At least 1 Logical Operator && , || or !
+e.  Must contain at least 3 user prompts that are used in calculations
+f.  All prompts must be validated
+
+Instructions
+
+ d. Continuously commit your files to your GitHub repo throughout the development
+    process.  You should have a minimum of 5 meaningful commits for this assignment.
+ e. Your code should give the user meaningful output.  So, after your calculations are
+    complete, your code should report back to the user the final values with a console.log().
+    This should contain the variables that you calculated and a concatenation text string
+    that describes the value. e.g.  console.log(“The area of the rectangle is “ +calcArea+ ”!”);
+ f. After your code is complete, make sure you test your final files.  Try typing in
+    different values to see what you get.  Put these test values in a multi-lined comment
+    at the bottom of your js file. e.g. /*  I typed in 5 for length and 6 for height and my
+    calculator gave me 30 for the area.*/
