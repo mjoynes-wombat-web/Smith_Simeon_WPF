32d9e4e2e14db4f24d7959218b1d3f184cdc5e4a
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-23 12:11:37 -0700

    Functions Worksheet: Stung
    Calculating the number of stings it would take to kill an animal based
    on it's weight.

--- Activities/Functions_worksheet/js/script.js
+++ Activities/Functions_worksheet/js/script.js
@@ -26,3 +26,24 @@ var circleC2 = calcCircumference(13); //Calling the function and defining the re
 console.log("The circumference of the first circle is " + circleC1 + "."); //Outputting the value.
 
 console.log("The circumference of the second circle is " + circleC2 + "."); //Outputting the value.
+
+
+/*STUNG*/
+
+var weight = Number(prompt("How much does the animal weight?"));
+
+//Function Setup
+
+function stingsForDeath (w) {           //Defining the function and parameters.
+    stingsPerPound = 8.666666667;       //Defining the number of strings per pound the results in death.
+    deathStings = stingsPerPound * w;   //Calculating how many stings would cased death based on the person's weight.
+    return deathStings;                 //Returning the value.
+}
+
+//Function Call
+
+var numStingsForDeath = stingsForDeath(weight); //Calling the function and defining the returned value.
+
+//Output
+
+console.log("It would require " + numStingsForDeath + " to kill an animal weighing " + weight + " pounds."); //Outputting the value.
