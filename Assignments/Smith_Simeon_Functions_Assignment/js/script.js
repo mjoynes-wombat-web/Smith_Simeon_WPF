1b7297b058146b0729ffcb5828d321e88604b56c
ssmith-wombatweb <ssmith@wombatweb.us>
2015-03-24 16:16:18 -0700

    Function Assignments: Cleanup, Flowchart and Zip
    Final edits and cleanup, added flowchart and zip up project for
    submission on FSO.

--- Assignments/Smith_Simeon_Functions_Assignment/js/script.js
+++ Assignments/Smith_Simeon_Functions_Assignment/js/script.js
@@ -6,85 +6,24 @@ Functions Assignment
 March 23, 2013
  */
 
-/* INSTRUCTIONS
- a. At least one function.
- b. Any functions created must use arguments and parameters.
- c. A value must be returned for each function.
- d. You must not use “main code” variable inside of a function; you must pass values
-    in as arguments and out as returned values.
- e. All user prompts must be validated using a while loop.
-
- CRITERIA
- You will need to create code that returns random numbers for each group of lottery
- numbers.
- • Keep in mind that the regular Florida lottery has six random numbers between 1 and 53;
- the Powerball lottery has five random numbers between 1 and 59 and a Powerball
- between 1 and 35.
- • Each random number must be different; a lottery will never have two of the same
- number unless it is the final Powerball number.
- • You should have at least one prompt to ask the user if they want the numbers for the
- Florida lottery or for the Powerball lottery.
- o Each prompt should be validated using a while loop.
- • The result should appear in the browser's console and include an explanation of the
- result.
- o Good example of console.log: The volume of the sphere is 26 feet cubed.
- o Bad example of console.log: 26
- • Final output should use string concatenation.
- • Comment every line of code (describe what each line is doing in English). Do NOT just
- label sections of your code.
- • Your code must contain at least one function
- • Each function must use arguments and parameters.
- • A value must be returned for every function you create.
- • You must not use global (“main code”) variables inside of a function; you must pass
- them in as	arguments.
- */
-
 
 /*VARIABLES*/
 
 var lotteryNums,        //Setting up lottery number variable.
-    lotteryType,        //Setting up the lottery type variable.
-    floridaLottery,     //Setting up the Florida Lottery variable.
-    powerballLottery,   //Setting up the Powerball Lottery variable.
-    powerball;          //Setting up the Powerball variable.
+    lotteryType;        //Setting up the lottery type variable.
 
 
 /*FUNCTIONS*/
 
-//Random lottery number generation.
-
-function lotteryNumGen (low, up, qt) {  //Defining function name, and parameters for quantity, upper limit, and lower limit.
-    var lotNumber = [],                 //Setting up internal lottery number array.
-        randomNumGen;                   //Setting up internal random number variable.
-
-    for (var ln = 0; ln < qt; ln++) {   //For loop to generate random numbers based on qt parameter.
-        randomNumGen = Math.round(Math.random() * (up - low) + low); //Outputting a random number based on the upper  and lower limit parameters.
-
-        lotNumber[ln] = randomNumGen;   //Inserting the random number into the lottery number array.
-
-        for (var n = 0; ln > n; n++) {  //If the ln array number is greater then zero then validate and loop through any other numbers.
-            if (lotNumber[n] === lotNumber[ln]){    //If the two lottery numbers are equal then do the following.
-                lotNumber[ln] = Math.round(Math.random() * (up - low) + low);   //Re-randomizing the lottery number.
-
-                n--;    //Subtract 1 from n so that the number is checked again.
-                //console.log(lotNumber);
-                //console.log(n)
-            }
-            //console.log(n);
-        }
-    }
-
-    return lotNumber;                   //Returning lottery numbers.
-
-}
-
+//Lottery type prompt with validation.
 
 function lotteryTypePrompt (n){ //Defining function name and parameter for prompt limit.
 
     var florida = false,    //Boolean for internal florida lottery.
         powerball = false,  //Boolean for internal powerball lottery.
-        lottery = prompt("Please enter the type of lottery you would like:\n\nIf you would like the Florida Lottery please enter Florida.\n\nIf you would like the Powerball Lottery please enter Powerball."); //User prompt asking what type of lottery they wan.t
+        lottery;
 
+    lottery = prompt("Please enter the type of lottery you would like:\n\nIf you would like the Florida Lottery please enter Florida.\n\nIf you would like the Powerball Lottery please enter Powerball."); //User prompt asking what type of lottery they want.
 
     if (lottery === "Florida Lottery" || lottery === "florida lottery" || lottery === "Florida" || lottery === "florida"){                  //Set florida lottery true if parameters are met.
 
@@ -125,6 +64,32 @@ function lotteryTypePrompt (n){ //Defining function name and parameter for promp
 }
 
 
+//Random lottery number generation.
+
+function lotteryNumGen (low, up, qt) {  //Defining function name, and parameters for quantity, upper limit, and lower limit.
+    var lotNumber = [],                 //Setting up internal lottery number array.
+        randomNumGen;                   //Setting up internal random number variable.
+
+    for (var ln = 0; ln < qt; ln++) {   //For loop to generate random numbers based on qt parameter.
+        randomNumGen = Math.round(Math.random() * (up - low) + low); //Outputting a random number based on the upper  and lower limit parameters.
+
+        lotNumber[ln] = randomNumGen;   //Inserting the random number into the lottery number array.
+
+        for (var n = 0; ln > n; n++) {  //If the ln array number is greater then zero then validate and loop through any other numbers.
+            if (lotNumber[n] === lotNumber[ln]){    //If the two lottery numbers are equal then do the following.
+                lotNumber[ln] = Math.round(Math.random() * (up - low) + low);   //Re-randomizing the lottery number.
+
+                n--;    //Subtract 1 from n so that the number is checked again.
+
+            }
+        }
+    }
+
+    return lotNumber;                   //Returning lottery numbers.
+
+}
+
+
 /*MAIN CODE*/
 
 lotteryType = lotteryTypePrompt(5); //Make lotteryType the results of the lotteryTypePrompt function with a 5 prompt limit.
@@ -132,24 +97,13 @@ lotteryType = lotteryTypePrompt(5); //Make lotteryType the results of the lotter
 
 if (lotteryType === "Florida Lottery" || lotteryType === "florida lottery" || lotteryType === "Florida" || lotteryType === "florida"){                  //Set florida lottery true if parameters are met.
 
-    floridaLottery = true;
-
-}else if (lotteryType === "Powerball Lottery" || lotteryType === "powerball lottery" || lotteryType === "Powerball" || lotteryType === "powerball"){    //If florida lottery false then set powerball lottery true if parameters are met.
-
-    powerballLottery = true;
-
-}
-
-
-if (floridaLottery){    //If Florida Lottery is true run the following.
-
     console.log("You chose the Florida Lottery.");  //Display this message if true.
 
     lotteryNums = lotteryNumGen(1, 53, 6);          //Make lotteryNums the results of lotteryNumGen with a low of 1, an upper of 53 and a total of 6 numbers.
 
     console.log("Your numbers are " + lotteryNums[0] + " " + lotteryNums[1] + " "+ lotteryNums[2] + " " + lotteryNums[3] + " " + lotteryNums[4] + " " + lotteryNums[5] + ".");  //Display the lottery numbers.
 
-}else if (powerballLottery){    //If Florida Lottery false and Powerball Lottery true run the following.
+}else if (lotteryType === "Powerball Lottery" || lotteryType === "powerball lottery" || lotteryType === "Powerball" || lotteryType === "powerball"){    //If florida lottery false then powerball lottery has to be true because of previous checks so do the following.
 
     console.log("You chose the Powerball Lottery.");    //Display this mesage of true.
 
